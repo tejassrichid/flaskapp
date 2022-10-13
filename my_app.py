7ed8eff76d05af498fbee7c2c687497a5ab8e168
tejassrichid <115620688+tejassrichid@users.noreply.github.com>
2022-10-13 16:50:23 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -18,24 +18,24 @@ import pytesseract
 app = Flask(__name__)
 #app.debug = True
 
-class PrefixMiddleware(object):
-#class for URL sorting 
-    def __init__(self, app, prefix=''):
-        self.app = app
-        self.prefix = prefix
+# class PrefixMiddleware(object):
+# #class for URL sorting 
+#     def __init__(self, app, prefix=''):
+#         self.app = app
+#         self.prefix = prefix
 
-    def __call__(self, environ, start_response):
-        #in this line I'm doing a replace of the word flaskredirect which is my app name in IIS to ensure proper URL redirect
-        if environ['PATH_INFO'].lower().replace('/flaskapp','').startswith(self.prefix):
-            environ['PATH_INFO'] = environ['PATH_INFO'].lower().replace('/flaskapp','')[len(self.prefix):]
-            environ['SCRIPT_NAME'] = self.prefix
-            return self.app(environ, start_response)
-        else:
-            start_response('404', [('Content-Type', 'text/plain')])            
-            return ["This url does not belong to the app.".encode()]
+#     def __call__(self, environ, start_response):
+#         #in this line I'm doing a replace of the word flaskredirect which is my app name in IIS to ensure proper URL redirect
+#         if environ['PATH_INFO'].lower().replace('/flaskapp','').startswith(self.prefix):
+#             environ['PATH_INFO'] = environ['PATH_INFO'].lower().replace('/flaskapp','')[len(self.prefix):]
+#             environ['SCRIPT_NAME'] = self.prefix
+#             return self.app(environ, start_response)
+#         else:
+#             start_response('404', [('Content-Type', 'text/plain')])            
+#             return ["This url does not belong to the app.".encode()]
 
 
-app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='')
+# app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='')
 
 @app.route('/test')
 def test():
